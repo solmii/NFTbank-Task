1d24a8b2544ec739d0f8c223956ec4c8d636a4b5
solmii <rkdthfal4759@naver.com>
2020-08-25 23:54:10 +0900

    [Add]ChampCounter component 구현

--- src/data/userChampList.js
+++ src/data/userChampList.js
@@ -94,3 +94,30 @@ export const userChampListData = [
     },
   },
 ];
+
+export const counterChampData = [
+  {
+    counterChampId: 0,
+    counterChampName: 'Fiora',
+    counterChampImg: 'https://blog.kakaocdn.net/dn/bBU16C/btqCU62Ywpl/E8j3VJfRrAQdzkFkisnOwK/img.png',
+    numOfPlays: 3,
+    numOfWins: 0,
+    numOfDefeats: 3,
+  },
+  {
+    counterChampId: 1,
+    counterChampName: 'Yorick',
+    counterChampImg: 'https://images.kbench.com/kbench/article/2015_05/k150671p1n1.jpg',
+    numOfPlays: 5,
+    numOfWins: 2,
+    numOfDefeats: 3,
+  },
+  {
+    counterChampId: 2,
+    counterChampName: 'Cassiopeia',
+    counterChampImg: 'https://t1.daumcdn.net/cfile/tistory/262B9C4555066DCE35',
+    numOfPlays: 3,
+    numOfWins: 1,
+    numOfDefeats: 2,
+  },
+];
